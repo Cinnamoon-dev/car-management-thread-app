be3116f6327c05aeb52ced75a3874e52b708654e
PedroH183 <pedro_freires18@outlook.com>
2024-03-25 16:03:01 -0300

    feat: implementation controlPanel logic.

--- Libs/View/Components/ControlPanel.java
+++ Libs/View/Components/ControlPanel.java
@@ -1,64 +1,31 @@
 package Libs.View.Components;
 
-import Libs.OriginSideEnum;
-
-import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 
-class BotaoInsertVehicle extends JButton implements ActionListener{
+public class ControlPanel extends JPanel{
 
     /*
-    * Classe responsável por representar visualmente os botões de adição de veiculos.
-    * Vai representar duas opções os carros que irão ser criados na esquerdas e direitas.
+    * Classe responsável por criar o box que engloba o
     * */
 
-    Character origin;
+    public FormInsertVehicle formInsertVehicle;
 
-    public BotaoInsertVehicle(OriginSideEnum org, String title){
-        this.setText(title);
-        this.origin = org.valor;
-        this.addActionListener(this::actionPerformed);
-    }
+    public ControlPanel(int boardWidth) {
+        this.setBackground(Color.lightGray);
+        this.setPreferredSize(new Dimension(boardWidth, 165));
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        /*
-         * Abrir um formulario solicitando as seguintes informações
-         *   tempo de travessia
-         *   tempo de permanencia
-         * */
+        this.formInsertVehicle = new FormInsertVehicle(boardWidth);
 
-        System.out.println("Button Clicked " + this.getText());
-        FormInsertVehicle insertVehicle = new FormInsertVehicle(this.origin);
-        insertVehicle.setContentPane(insertVehicle.panelMain);
-        insertVehicle.setVisible(true);
-    }
-}
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-
-public class ControlPanel extends JPanel {
-    /*
-    * Classe que representa o controle de opções para inserção de novos carros na ponte.
-    * Funcionalidade : Da a opção de inserir veiculos, na direita ou na esquerda, ao usuario.
-    * */
-
-    int boardWith;
-    int boardHeight;
-
-    public ControlPanel(int boardWith, int boardHeight) {
-        this.boardWith = boardWith;
-        this.boardHeight = boardHeight;
-
-        setBackground(Color.lightGray);
-        this.setPreferredSize(new Dimension(boardWith, boardHeight));
-
-        BotaoInsertVehicle buttonL = new BotaoInsertVehicle(OriginSideEnum.Left, "Insert Left");
-        BotaoInsertVehicle buttonR = new BotaoInsertVehicle(OriginSideEnum.Rigth, "Insert Right");
-
-        this.add(buttonL);
-        this.add(buttonR);
+        this.add(formInsertVehicle, gbc);
+        this.setVisible(true);
     }
 }
